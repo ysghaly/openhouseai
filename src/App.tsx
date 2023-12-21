4b8f02fcd596a531174b31b7745ce1033be6bc18
ysghaly <youssefadly47@yahoo.com>
2023-12-21 15:51:25 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+
 
 function App() {
+
+  const [communities, setCommunities] = useState("");
+  useEffect(() => {
+
+       
+    fetch("https://storage.googleapis.com/openhouse-ai-fe-coding-test/communities.json", {
+      method: 'GET',
+      redirect: 'follow',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      }
+    }) 
+      .then(response => response.text())
+      .then(result => setCommunities(result))
+      .catch(error => console.log('error', error));
+
+},[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          {communities}
         </p>
         <a
           className="App-link"
---
+++ src/getHome.ts
@@ -0,0 +1,42 @@
+
+
+const getHome = () =>{
+
+        
+  
+
+    // getCommunities();
+    console.log('getTime()');
+};
+
+interface Community {
+    id: string
+    name: string
+    imgUrl: string
+    group: string
+  }
+
+ 
+//   async function getCommunities() {
+
+  
+//     var requestOptions = {
+//         method: 'GET',
+//         redirect: 'follow'
+//       };
+      
+//       fetch("https://storage.googleapis.com/openhouse-ai-fe-coding-test/communities.json", {
+//         method: 'GET',
+//         mode: 'no-cors',
+//         redirect: 'follow'
+//       })
+//         .then(response => response.text())
+//         .then(result => console.log(result))
+//         .catch(error => console.log('error', error));
+//   }
+  
+
+  
+
+
+export default getHome;
