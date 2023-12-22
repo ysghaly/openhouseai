b1544a5af86354a9cd16f21f853dc7c3530d89b4
ysghaly <youssefadly47@yahoo.com>
2023-12-21 17:56:52 -0700

    Used a free CORS proxy server to handle issue with API call. Added some CSS to create overall layout.

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,106 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from "react";
+import stockPhoto from './assets/AdobeStock_642180565_Preview.jpeg'
 
 
 function App() {
 
-  const [communities, setCommunities] = useState("");
+
+  const [communities, setCommunities] = useState([{id:"", name: "", imgUrl: "", group: ""}]);
+  const [homes, setHomes] = useState([{id:"", communityId: "", price: 0, area: 0, type: ""}]);
   useEffect(() => {
 
        
-    fetch("https://storage.googleapis.com/openhouse-ai-fe-coding-test/communities.json", {
+    fetch("https://corsproxy.io/?https://storage.googleapis.com/openhouse-ai-fe-coding-test/communities.json", {
       method: 'GET',
       redirect: 'follow',
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Access-Control-Allow-Methods': "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        'Origin': 'none'
       }
     }) 
-      .then(response => response.text())
+      .then(response => response.json())
       .then(result => setCommunities(result))
+      .catch(error => console.log('error', error));
+       
+    fetch("https://corsproxy.io/?https://storage.googleapis.com/openhouse-ai-fe-coding-test/homes.json", {
+      method: 'GET',
+      redirect: 'follow',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Access-Control-Allow-Methods': "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        'Origin': 'none'
+      }
+    }) 
+      .then(response => response.json())
+      .then(result => setHomes(result))
       .catch(error => console.log('error', error));
 
 },[])
 
+
+
+
+
+  let community_names: any[] = [];
+
+  getCommunityInfo();
+
+  function getCommunityInfo(){
+    
+    communities.forEach((community, index) => {
+      community_names.push(
+        <div key={index} className='community'>
+          <h2 className='community-name'>{community.name}</h2> 
+          {(getAveragePrice(community.id) > 0) && <h3 className='community-price'>Average Price: $ {getAveragePrice(community.id).toLocaleString()}</h3>}
+          {! (getAveragePrice(community.id) > 0) && <h3 className='community-price'>Average Price: N/A</h3>}
+          <img alt="No Photo" className='community-image' src={community.imgUrl} 
+            onError={
+              ({ currentTarget }) => {
+                currentTarget.src=stockPhoto;
+              }
+            }
+          />
+        </div>
+      );
+    });
+  }
+
+  function swapImage(image: HTMLImageElement){
+    image.src=stockPhoto;
+  }
+
+  function getAveragePrice(id: string): number{
+    var avg_price = 0;
+    var total_price = 0;
+    var num_homes = 0;
+    homes.forEach((home, index) => {
+      
+      if (home.communityId == id){
+        total_price = total_price + home.price;
+        num_homes = num_homes + 1;
+      }
+    });
+
+    if (num_homes > 0)
+      avg_price = total_price / num_homes;
+
+    return total_price;
+
+  }
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          {communities}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Community List</h1>
+        { community_names }
     </div>
   );
 }
--- src/getHome.ts
+++ src/getHome.ts
@@ -4,9 +4,10 @@ const getHome = () =>{
 
         
   
-
     // getCommunities();
-    console.log('getTime()');
+    // getHomes();
+    // console.log(getCommunities());
+    // console.log(getHomes());
 };
 
 interface Community {
@@ -16,24 +17,54 @@ interface Community {
     group: string
   }
 
+interface Home{
+    id: string
+    communityId: string
+    price: number
+    area: number
+    type: string
+}
  
-//   async function getCommunities() {
+// var communities: Community[] = [];
+// var homes: Home[] = [];
+// function getCommunities(){
+
+//     var home: Home[] = [{id:"",communityId:"",price:0,area:0,type:""}];
+  
+//     fetch("https://corsproxy.io/?https://storage.googleapis.com/openhouse-ai-fe-coding-test/communities.json", {
+//         method: 'GET',
+//         redirect: 'follow',
+//         headers: {
+//           'Access-Control-Allow-Origin': '*',
+//           'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+//           'Access-Control-Allow-Methods': "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+//           'Origin': 'none'
+//         }
+//       }) 
+//         .then(response => response.json())
+//         .then(result =>  {home = result})
+//         .catch(error => console.log('error', error));
+
+//         return home;
+// }
+// function getHomes() {
 
   
-//     var requestOptions = {
+//     fetch("https://corsproxy.io/?https://storage.googleapis.com/openhouse-ai-fe-coding-test/homes.json", {
 //         method: 'GET',
-//         redirect: 'follow'
-//       };
-      
-//       fetch("https://storage.googleapis.com/openhouse-ai-fe-coding-test/communities.json", {
-//         method: 'GET',
-//         mode: 'no-cors',
-//         redirect: 'follow'
-//       })
-//         .then(response => response.text())
-//         .then(result => console.log(result))
+//         redirect: 'follow',
+//         headers: {
+//           'Access-Control-Allow-Origin': '*',
+//           'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+//           'Access-Control-Allow-Methods': "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+//           'Origin': 'none'
+//         }
+//       }) 
+//         .then(response => response.json())
+//         .then(result => {return result})
 //         .catch(error => console.log('error', error));
-//   }
+
+// }
   
 
   
